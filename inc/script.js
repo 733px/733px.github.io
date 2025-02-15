d8d07e7c17656349b903ff9eed346d0b6e47ac2f
733px <126036585+733px@users.noreply.github.com>
2025-02-14 20:11:37 -0500

    Update script.js

--- inc/script.js
+++ inc/script.js
@@ -1,16 +1,1 @@
-function toggleScreen() {
-    let body = document.body;
-    let topRightButton = document.querySelector('.top-right');
-    let centerButton = document.querySelector('.center');
-
-    if (body.style.backgroundColor === 'black') {
-        body.style.backgroundColor = 'white';
-        topRightButton.style.display = 'block';
-        centerButton.style.display = 'none';
-    } else {
-        body.style.backgroundColor = 'black';
-        topRightButton.style.display = 'none';
-        centerButton.style.display = 'block';
-        centerButton.style.color = 'white';
-    }
-}
+ 
